9655a9c2871ed2a6e66d0855f57a10fcf9e4df91
black-hole-studio <jerg0416@gmail.com>
2024-08-13 19:30:33 +0800

    Add files via upload

---
+++ inter-knot-main/components/main-container.vue
@@ -0,0 +1,181 @@
+<template>
+  <main>
+    <div class="center" v-if="needUpdata">
+      <a href="https://greasyfork.org/zh-CN/scripts/502874" class="link">
+        请更新“绳网小助手”，最新版本为 1.10.0
+      </a>
+    </div>
+    <div class="center" v-else-if="needInstall">
+      <a href="https://greasyfork.org/zh-CN/scripts/502874" class="link">
+        请安装“绳网小助手”
+      </a>
+    </div>
+    <ClientOnly v-else>
+      <Waterfall
+        :list="list"
+        background-color="transparent"
+        :width="256"
+        img-selector="img"
+        ref="waterfall"
+        :breakpoints="[
+          { width: 256, cols: 1 },
+          { width: 512, cols: 2 },
+          { width: 768, cols: 3 },
+          { width: 1024, cols: 4 },
+          { width: 1280, cols: 5 },
+          { width: 1440, cols: 6 },
+          { width: 1600, cols: 7 },
+          { width: 1920, cols: 8 },
+        ]"
+      >
+        <template #item="{ item }">
+          <Card
+            :key="item.article.url"
+            :article="item.article"
+            @click="clickHandle(item.article)"
+            @resize="waterfall.renderer()"
+          />
+        </template>
+      </Waterfall>
+    </ClientOnly>
+    <popup-article
+      @close="closeHandle"
+      :show="showPopup"
+      :article="curArticle"
+    />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { Waterfall } from "vue-waterfall-plugin-next";
+import "vue-waterfall-plugin-next/dist/style.css";
+
+const store = useConfigStore();
+const { data } = storeToRefs(store);
+const needUpdata = ref(false);
+const needInstall = ref(false);
+const showPopup = ref(false);
+const curArticle = ref<Article>();
+const list = computed(() =>
+  data.value.map((e) => ({
+    src: e.cover,
+    article: e,
+  }))
+);
+const waterfall = ref();
+
+onMounted(async () => {
+  window.addEventListener("popstate", async function () {
+    const url = new URL(location.href);
+    if (url.searchParams.has("article")) {
+      showPopup.value = true;
+      const str = url.searchParams.get("article")!;
+      console.log(str);
+      try {
+        const res = await getDiscussion(parseInt(str));
+        curArticle.value = res;
+        this.document.title = res.title;
+      } catch (e) {
+        useNuxtApp().$toast.error("获取文章失败");
+      }
+    } else {
+      showPopup.value = false;
+    }
+  });
+  const url = new URL(location.href);
+  if (url.searchParams.has("article")) {
+    showPopup.value = true;
+    const str = url.searchParams.get("article")!;
+    console.log(str);
+    try {
+      const res = await getDiscussion(parseInt(str));
+      curArticle.value = res;
+      document.title = res.title;
+    } catch (e) {
+      useNuxtApp().$toast.error("获取文章失败");
+    }
+  }
+});
+
+function clickHandle(article: Article) {
+  curArticle.value = article;
+  showPopup.value = true;
+  const url = new URL(location.href);
+  url.searchParams.set("article", article.number + "");
+  history.pushState(null, "", url);
+  document.title = article.title;
+}
+
+function closeHandle() {
+  showPopup.value = false;
+  const url = new URL(location.href);
+  url.searchParams.delete("article");
+  history.pushState(null, "", url);
+  document.title = "绳网";
+}
+
+onMounted(async () => {
+  setTimeout(() => {
+    if (typeof window.getUserInfo !== "function") {
+      needInstall.value = true;
+    } else if (window.version !== "1.10.0") {
+      needUpdata.value = true;
+    }
+  }, 2000);
+  if (typeof window.run === "undefined") window.run = [];
+  window.run.push(async () => {
+    if (window.version !== "1.10.0") {
+      needUpdata.value = true;
+      return;
+    }
+    useInfiniteScroll(
+      window,
+      async () => {
+        if (store.hasNextPage === false) return;
+        console.log("scroll");
+        try {
+          const res = await getAllDiscussions();
+          store.hasNextPage = res.hasNextPage;
+          data.value.push(...res.discussions);
+          removeDuplicateArticle(data.value);
+        } catch (e) {
+          console.error(e);
+          useNuxtApp().$toast.error("获取文章失败");
+        }
+      },
+      { distance: 1000 }
+    );
+  });
+});
+</script>
+
+<style scoped lang="less">
+main {
+  position: relative;
+  margin-top: 100px;
+  padding: 28px;
+  background: #121212;
+  min-height: calc(100vh - 100px);
+  margin-left: auto;
+  margin-right: auto;
+
+  @media (max-width: 1150px) {
+    & {
+      margin-top: 80px;
+      min-height: calc(100vh - 80px);
+    }
+  }
+
+  .link {
+    color: #66ccff;
+  }
+
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 24px;
+  }
+}
+</style>
---
+++ inter-knot-main/components/refresh-btn.vue
@@ -0,0 +1,43 @@
+<template>
+  <Teleport to="body">
+    <div class="refresh" title="刷新" @click="$emit('refresh')">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M5.463 4.433A9.96 9.96 0 0 1 12 2c5.523 0 10 4.477 10 10c0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228zm13.074 15.134A9.96 9.96 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772z"
+        />
+      </svg>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+defineEmits(["refresh"]);
+const store = useConfigStore();
+</script>
+
+<style scoped lang="less">
+.refresh {
+  position: fixed;
+  bottom: 100px;
+  right: 40px;
+  color: unset;
+  background: #222222;
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  box-shadow: inset 0 2px 2px #313431, inset 0 -2px 2px #181818,
+    0 2px 20px #313431;
+  z-index: 50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
